cf52e46b4ede985fadc1817c03b6ec01c998f681
Ankush Mehta <ankushmehta243@gmail.com>
2025-02-27 12:22:26 +0530

    Data Transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,6 +3,7 @@ import sys
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from data_transformation import DataTransformation ,DataTransformationConfig
 
 # Ensure Python can find logger.py and exception.py
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
@@ -48,4 +49,7 @@ class DataIngestion:
         
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
